da90efdfdf291d51628f9b29f772c5dd2428f02a
Sakib Hadziavdic <sakib@sake.ba>
2025-07-16 16:38:05 +0200

    Add squery

--- api/src/com/example/petstore/api/models/Address.scala
+++ api/src/com/example/petstore/api/models/Address.scala
@@ -4,4 +4,5 @@ import java.util.UUID
 import org.typelevel.jawn.ast.JValue
 import ba.sake.tupson.*
 import ba.sake.validson.Validator
-case class Address(street: Option[String], city: Option[String], state: Option[String], zip: Option[String]) derives JsonRW
+case class Address(street: Option[String], city: Option[String], state: Option[String], zip: Option[String])
+    derives JsonRW
--- api/src/com/example/petstore/api/models/Order.scala
+++ api/src/com/example/petstore/api/models/Order.scala
@@ -4,5 +4,12 @@ import java.util.UUID
 import org.typelevel.jawn.ast.JValue
 import ba.sake.tupson.*
 import ba.sake.validson.Validator
-case class Order(id: Option[Long], petId: Option[Long], quantity: Option[Int], shipDate: Option[Instant], status: Option[OrderStatus], complete: Option[Boolean]) derives JsonRW
+case class Order(
+    id: Option[Long],
+    petId: Option[Long],
+    quantity: Option[Int],
+    shipDate: Option[Instant],
+    status: Option[OrderStatus],
+    complete: Option[Boolean]
+) derives JsonRW
 enum OrderStatus derives JsonRW { case placed, approved, delivered }
